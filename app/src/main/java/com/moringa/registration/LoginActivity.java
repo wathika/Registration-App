d37334fd38119d938f1ef7df067b0b6edcfc1514
wathika <vcwathika@gmail.com>
2015-09-12 14:57:30 +0100

    m

--- app/src/main/java/com/moringa/registration/LoginActivity.java
+++ app/src/main/java/com/moringa/registration/LoginActivity.java
@@ -1,11 +1,20 @@
 package com.moringa.registration;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
 
 
 public class LoginActivity extends Activity {
@@ -25,6 +34,51 @@ public class LoginActivity extends Activity {
         mPassword = (EditText)findViewById(R.id.passwordLoginTextBox);
         mLoginBtn = (Button)findViewById(R.id.loginButton);
         mCreateAccountBtn = (Button)findViewById(R.id.createAccountButton);
+
+
+        //listen to when mLoginBtn  is clicked
+        mLoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //get the user inputs and convert to string
+                String username = mUsername.getText().toString().trim();
+                String password = mPassword.getText().toString().trim();
+
+
+                //login the user using parse(parse checks for User if the user is logged in)
+                ParseUser.logInInBackground(username, password, new LogInCallback() {
+                    @Override
+                    public void done(ParseUser parseUser, ParseException e) {
+                        if (e == null) {
+                            //success: we have a user loggedin
+                            Toast.makeText(LoginActivity.this, "Welcome Back.", Toast.LENGTH_LONG).show();
+
+                            //take user to homepage
+                            Intent takeUserHome = new Intent(LoginActivity.this, HomepageActivity.class);
+                            startActivity(takeUserHome);
+
+                        } else {
+                            //sorry there was a problem logging in
+                            AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+                            builder.setMessage(e.getMessage());
+                            builder.setTitle("Sorry!");
+                            builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    //close the dialog
+                                    dialog.dismiss();
+                                }
+                            });
+                            AlertDialog dialog = builder.create();
+                            dialog.show();
+
+                        }
+
+                    }
+                });
+            }
+        });
+
 
 
     }
